53505816afef55238356d202a99f1b054bf00be0
taniagoia11 <82938400+taniagoia11@users.noreply.github.com>
2022-11-28 20:07:39 +0100

    read data

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from jbi100_app.main import app
 from jbi100_app.views.menu import make_menu_layout
 from jbi100_app.views.scatterplot import Scatterplot
 
-from dash import html
+from dash import html, dcc
 import pandas as pd
 import plotly.express as px
 from dash.dependencies import Input, Output
@@ -10,48 +10,17 @@ from dash.dependencies import Input, Output
 
 if __name__ == '__main__':
     # Create data
-    df = px.data.iris();;
-    scatterplot1 = Scatterplot("Scatterplot 1", 'price', 'beds', df)
-    scatterplot2 = Scatterplot("Scatterplot 2", 'petal_length', 'petal_width', df)
-    # Instantiate custom views
-    app.layout = html.Div(
-        id="app-container",
-        children=[
-            # Left column
-            html.Div(
-                id="left-column",
-                className="three columns",
-                children=make_menu_layout()
+    df = pd.read_csv(R"C:\Users\tania\Desktop\year 3 q2\visualization\airbnb_open_data.csv", low_memory=False)
+
+    app.layout = html.Div(children=[
+
+            html.H1(children="hello"),
+            html.Div(children="description"
             ),
-
-            # Right column
-            html.Div(
-                id="right-column",
-                className="nine columns",
-                children=[
-                    scatterplot1,
-                    scatterplot2
-                ],
-            ),
-        ],
-    )
-
-    # Define interactions
-    @app.callback(
-        Output(scatterplot1.html_id, "figure"), [
-        Input("select-color-scatter-1", "value"),
-        Input(scatterplot2.html_id, 'selectedData')
-    ])
-    def update_scatter_1(selected_color, selected_data):
-        return scatterplot1.update(selected_color, selected_data)
-
-    @app.callback(
-        Output(scatterplot2.html_id, "figure"), [
-        Input("select-color-scatter-2", "value"),
-        Input(scatterplot1.html_id, 'selectedData')
-    ])
-    def update_scatter_2(selected_color, selected_data):
-        return scatterplot2.update(selected_color, selected_data)
-
-
-    app.run_server(debug=False, dev_tools_ui=False)
+            dcc.Graph(
+                id="graph",
+                figure=fig
+            )
+        ])
+    if __name__ == '__main__':
+        app.run_server(debug=True)
